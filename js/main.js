1eaed2610d4f769dcc7e1da185061c94118b7310
Alberto <alberto.abate87@gmail.com>
2020-10-20 15:59:47 +0200

    inizio

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+// Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+
+// stampare i numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+
+// stampare al posto dei multipli di 3 la parola "Fizz"
+if (i % 3 == 0) {
+  console.log('Fizz');
+} else {
+  console.log(i);
+}
+// stampare al posto dei multipli di 5 la parola "Buzz"
+
+// stampare al posto dei numeri che sono sia multipli di 3 che di 5 la parola "FizzBuzz"
+}
